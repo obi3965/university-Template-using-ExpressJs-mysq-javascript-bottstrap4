25859d25e76e4dae3a4d7b56fe52c4a3d39d4b0e
obi3965 <5571142@RTS-365.dk>
2019-11-30 22:12:24 +0100

    Update main.js

--- public/Js/main.js
+++ public/Js/main.js
@@ -74,17 +74,17 @@ document.addEventListener('DOMContentLoaded', (event)=>{
    
    
 //#################################TABS PANEL###########################
-function onTabClick(event) {
-  let activeTabs = document.querySelectorAll('.active');
+// function onTabClick(event) {
+//   let activeTabs = document.querySelectorAll('.active');
   
-  activeTabs.forEach(function(tab) {
-    tab.className = tab.className.replace('active', '');
-  });
+//   activeTabs.forEach(function(tab) {
+//     tab.className = tab.className.replace('active', '');
+//   });
   
-  // activate new tab and panel
-  event.target.parentElement.className += ' active';
-  document.getElementById(event.target.href.split('#')[1]).className += ' active';
-}
+//   // activate new tab and panel
+//   event.target.parentElement.className += ' active';
+//   document.getElementById(event.target.href.split('#')[1]).className += ' active';
+// }
 
 const element = document.getElementById('nav-tab');
 
